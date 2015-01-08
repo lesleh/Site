8b59f45eaa77aca34c1f6dd6c346310646bfa138
Leslie Hoare <iam@lesleh.co.uk>
2015-01-08 10:40:37 +0000

    Clean up posts layouts and hide admin stuff unless logged in.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate, except: [:index, :show]
   before_action :set_default_title
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
