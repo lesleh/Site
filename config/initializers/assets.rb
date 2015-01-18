9b1b84c3b28c3bda38623f57db80dc5125f4263a
Leslie Hoare <iam@lesleh.co.uk>
2015-01-18 18:11:24 +0000

    Fix the modernizr script tag.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,11 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+# Bower assets
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+
+Rails.application.config.assets.precompile += %w( modernizr/modernizr.js )
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
