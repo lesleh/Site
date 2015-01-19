e6ad39d76ae9d488f047ad8fee7f360e45d5c6d8
Leslie Hoare <iam@lesleh.co.uk>
2015-01-19 16:08:00 +0000

    Make flash notices use Bootstrap classes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
 
+  def bootstrap_class_for flash_type
+    {
+        success: "alert-success",
+        error: "alert-danger",
+        alert: "alert-warning",
+        notice: "alert-info"
+    }[flash_type.to_sym] || 'alert-' + flash_type.to_s
+  end
+
   def logged_in?
     true if session[:login]
   end
