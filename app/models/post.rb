eee5ac61cbda921c66ec77ec3b703f53afb5a9b7
Leslie Hoare <iam@lesleh.co.uk>
2015-01-22 03:33:55 +0000

    Add Post validations to make tests pass.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
 
+  validates_presence_of :title, :body
+
   def first_paragraph
     body[/<p>.*?<\/p>/]
   end
