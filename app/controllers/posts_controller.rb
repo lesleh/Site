ce408b3d094a06de6756217c4ccbcda2ac608102
Leslie Hoare <iam@lesleh.co.uk>
2015-01-29 18:09:16 +0000

    DRY controllers.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate, except: [:index, :show]
   before_action :set_default_title
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  respond_to :html, :json
 
   # GET /posts
   # GET /posts.json
@@ -36,39 +37,25 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    flash[:success] = "Post was successfully created." if @post.save
+    respond_with(@post)
   end
 
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.update(post_params)
+      flash[:success] = 'Post was successfully updated.'
     end
+    respond_with(@post)
   end
 
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:success] = "Post deleted"
+    respond_with @post
   end
 
   private
