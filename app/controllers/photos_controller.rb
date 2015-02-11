5842fe7b5d6842136dd800bef27379a82311f99b
Leslie Hoare <iam@lesleh.co.uk>
2015-02-11 20:18:00 +0000

    Loop to get all photos ever. Slow-ish but it's cached for 24 hours.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,22 +7,35 @@ class PhotosController < ApplicationController
   private
 
   def get_photos
-    Rails.cache.fetch("instagram_photos", expires_in: 1.hour) do
+    Rails.cache.fetch("instagram_photos", expires_in: 24.hours) do
       get_photos_real
     end
   end
 
   def get_photos_real
     client = Instagram.client(:access_token => Rails.application.secrets.instagram_access_token)
-    client.user_recent_media({:count => 1000}).map do |e|
-      caption = e.caption ? e.caption.text : nil
-      {
-          caption: caption,
-          link: e.link,
-          image: e.images.standard_resolution.url.gsub(/^http/, 'https'),
-          thumbnail: e.images.thumbnail.url.gsub(/^http/, 'https')
-      }
+
+    photos = []
+    next_max_id = nil
+    loop do
+      data = client.user_recent_media('self', count: 100, max_id: next_max_id)
+      photos.concat( data.map {|e| parse_photo(e)} )
+
+      next_max_id = data.pagination.next_max_id
+      break if next_max_id == nil
     end
+
+    photos
+  end
+
+  def parse_photo(photo)
+    caption = photo.caption ? photo.caption.text : nil
+    {
+        caption: caption,
+        link: photo.link,
+        image: photo.images.standard_resolution.url.gsub(/^http/, 'https'),
+        thumbnail: photo.images.thumbnail.url.gsub(/^http/, 'https')
+    }
   end
 
 end
