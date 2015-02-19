08992ba668359b1a6907749472c8ea64a2dd00c0
Leslie Hoare <iam@lesleh.co.uk>
2015-02-19 07:25:50 +0000

    Pun unit tests.

--- spec/models/pun_spec.rb
+++ spec/models/pun_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Pun, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "creates a pun" do
+    text = "Velcro, what a rip off."
+    pun = Pun.create!(text: text)
+    expect(pun.text).to eq(text)
+  end
+
+  it "requires a text" do
+    pun = Pun.create
+    expect(pun.valid?).to be(false)
+  end
 end
