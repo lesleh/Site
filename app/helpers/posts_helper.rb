026ffbc04aecb750c89a2f673ebe6aa2788b0781
Leslie Hoare <iam@lesleh.co.uk>
2015-03-10 10:22:26 +0000

    Helper for months archive. Sort of.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -5,6 +5,15 @@ module PostsHelper
     link_to date.strftime("%B %Y"), blog_archive_path(date.year, '%02d' % date.month), *opts
   end
 
+  def post_months
+    Post.months.reverse.map do |k|
+      {
+          :title => k[:date].strftime("%B %Y"),
+          :count => k[:count]
+      }
+    end
+  end
+
   def tag_links(post)
     post.tags.map(&:name).map {|t| link_to t, blog_tag_path(t)}.join(', ').html_safe
   end
